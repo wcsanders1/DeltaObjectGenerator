2d2d6fccd0969f3bd3dba3fa256c3e992b75adc2
William Sanders <william.sanders076@gmail.com>
2019-03-20 07:00:12 -0400

    Add Newtonsoft lib and scaffold delta object generator for JObject

---
+++ src/DeltaObjectGenerator/Generators/DeltaObjectFromJObjectGenerator.cs
@@ -0,0 +1,26 @@
+﻿using DeltaObjectGenerator.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeltaObjectGenerator.Generators
+{
+    internal class DeltaObjectFromJObjectGenerator
+    {
+        public static List<DeltaObject> GetDeltaObjects<T>(T originalObject, JObject jObject)
+        {
+            if (originalObject == null)
+            {
+                throw new ArgumentNullException(nameof(originalObject));
+            }
+
+            if (jObject == null)
+            {
+                throw new ArgumentNullException(nameof(jObject));
+            }
+
+            return null;
+        }
+    }
+}
