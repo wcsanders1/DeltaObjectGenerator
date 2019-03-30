d91ac4c7cc9be4d0f8dda83917d2827becae210f
William Sanders <william.sanders076@gmail.com>
2019-03-30 18:55:27 -0400

    Add class with interface and methods to get delta objects

---
+++ src/DeltaObjectGenerator/Generators/DeltaObjectEngine.cs
@@ -0,0 +1,66 @@
+﻿using DeltaObjectGenerator.Extensions;
+using DeltaObjectGenerator.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace DeltaObjectGenerator.Generators
+{
+    /// <summary>
+    /// Interface offering methods returning <see cref="List{DeltaObject}"./>
+    /// </summary>
+    public interface IDeltaObjectEngine
+    {
+        /// <summary>
+        /// Returns a <see cref="List{DeltaObject}"/>, which will be empty if there are no deltas.
+        /// </summary>
+        /// <typeparam name="T">Any type.</typeparam>
+        /// <param name="originalObject">A type of <typeparamref name="T"/>.</param>
+        /// <param name="newObject">A type of <typeparamref name="T"/>.</param>
+        /// <returns><see cref="List{DeltaObject}"/></returns>
+        /// <exception cref="ArgumentNullException">Thrown when either arguments are <c>null</c>.</exception>
+        List<DeltaObject> GetDeltaObjects<T>(T originalObject, T newObject);
+
+        /// <summary>
+        /// Returns a <see cref="List{DeltaObject}"/>, which will be empty if there are no deltas.
+        /// </summary>
+        /// <typeparam name="T">Any type.</typeparam>
+        /// <param name="originalObject">A type of <typeparamref name="T"/>.</param>
+        /// <param name="jObject">A type of <see cref="JObject"/>.</param>
+        /// <returns><see cref="List{DeltaObject}"/></returns>
+        /// <exception cref="ArgumentNullException">Thrown when either arguments are <c>null</c>.</exception>
+        List<DeltaObject> GetDeltaObjects<T>(T originalObject, JObject jObject);
+    }
+
+    /// <summary>
+    /// A class offering functionality to generate a <see cref="List{DeltaObject}"/>
+    /// </summary>
+    public class DeltaObjectEngine : IDeltaObjectEngine
+    {
+        /// <summary>
+        /// Returns a <see cref="List{DeltaObject}"/>, which will be empty if there are no deltas.
+        /// </summary>
+        /// <typeparam name="T">Any type.</typeparam>
+        /// <param name="originalObject">A type of <typeparamref name="T"/>.</param>
+        /// <param name="newObject">A type of <typeparamref name="T"/>.</param>
+        /// <returns><see cref="List{DeltaObject}"/></returns>
+        /// <exception cref="ArgumentNullException">Thrown when either arguments are <c>null</c>.</exception>
+        public List<DeltaObject> GetDeltaObjects<T>(T originalObject, T newObject)
+        {
+            return originalObject.GetDeltaObjects(newObject);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="List{DeltaObject}"/>, which will be empty if there are no deltas.
+        /// </summary>
+        /// <typeparam name="T">Any type.</typeparam>
+        /// <param name="originalObject">A type of <typeparamref name="T"/>.</param>
+        /// <param name="jObject">A type of <see cref="JObject"/>.</param>
+        /// <returns><see cref="List{DeltaObject}"/></returns>
+        /// <exception cref="ArgumentNullException">Thrown when either arguments are <c>null</c>.</exception>
+        public List<DeltaObject> GetDeltaObjects<T>(T originalObject, JObject jObject)
+        {
+            return originalObject.GetDeltaObjects(jObject);
+        }
+    }
+}
---
+++ src/DeltaObjectGeneratorTests/Unit/DeltaObjectGeneratorUnitTests.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+using DeltaObjectGenerator.Generators;
+using DeltaObjectGeneratorTests.TestModels;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using DeltaObjectGenerator.Models;
+
+namespace DeltaObjectGeneratorTests.Unit
+{
+    public class DeltaObjectGeneratorUnitTests
+    {
+        [Trait("Category", "Unit")]
+        public class GetDeltaObjects_Object
+        {
+            [Fact]
+            public void ReturnsDeltaObjects_WhenCalled()
+            {
+                var originalCustomer = new TestCustomer
+                {
+                    FirstName = "original name"
+                };
+
+                var newCustomer = new TestCustomer
+                {
+                    FirstName = "new name"
+                };
+
+                var sut = new DeltaObjectEngine();
+
+                var deltaObjects = sut.GetDeltaObjects(originalCustomer, newCustomer);
+
+                Assert.Single(deltaObjects);
+                Assert.Equal(nameof(TestCustomer.FirstName), deltaObjects.First().PropertyName);
+                Assert.Equal(nameof(TestCustomer.FirstName), deltaObjects.First().PropertyAlias);
+                Assert.Equal(originalCustomer.FirstName, deltaObjects.First().OriginalValue);
+                Assert.Equal(newCustomer.FirstName, deltaObjects.First().NewValue);
+            }
+        }
+
+        [Trait("Category", "Unit")]
+        public class GetDeltaObjects_JObject
+        {
+            [Fact]
+            public void ReturnsDeltaObjects_WhenCalled()
+            {
+                var originalCustomer = new TestCustomer
+                {
+                    FirstName = "originalFirstName",
+                    LastName = "originalLastName"
+                };
+
+                var newCustomer = new
+                {
+                    FirstName = "newFirstName",
+                    LastName = "originalLastName"
+                };
+
+                var newCustomerJObj = JObject.FromObject(newCustomer);
+
+                var sut = new DeltaObjectEngine();
+
+                var deltaObjects = sut.GetDeltaObjects(originalCustomer, newCustomerJObj);
+
+                Assert.NotNull(deltaObjects);
+                Assert.Single(deltaObjects);
+                Assert.Equal(ConversionStatus.Valid, deltaObjects[0].ConversionStatus);
+                Assert.Equal(newCustomer.FirstName, deltaObjects[0].NewValue);
+            }
+        }
+    }
+}
