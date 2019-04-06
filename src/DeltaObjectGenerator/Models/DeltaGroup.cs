56a1475c6457171a8e3dd1cdf2ef4bacee51662e
William Sanders <william.sanders076@gmail.com>
2019-04-06 13:40:07 -0400

    Add DeltaGroup model for deltas calculated against a JObject

---
+++ src/DeltaObjectGenerator/Models/DeltaGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DeltaObjectGenerator.Models
+{
+    /// <summary>
+    /// A class containing a <see cref="List{DeltaObject}"/> for successfully converted properties 
+    /// and a <see cref="List{DeltaObject}"/> for unsuccessfully converted properties.
+    /// </summary>
+    public class DeltaGroup
+    {
+        /// <summary>
+        /// Indicates whether all, some, or none of the properties were successfully converted 
+        /// into their associated types.
+        /// </summary>
+        public GroupConversionStatus ConversionStatus { get; internal set; }
+
+        /// <summary>
+        /// Contains <see cref="DeltaObject"/>s with <see cref="ConversionStatus.Valid"/>.
+        /// </summary>
+        public List<DeltaObject> DeltaObjectsConversionSuccess { get; internal set; }
+
+        /// <summary>
+        /// Contains <see cref="DeltaObject"/>s with <see cref="ConversionStatus.Invalid"/>.
+        /// </summary>
+        public List<DeltaObject> DeltaObjectsConversionFail { get; internal set; }
+
+        internal DeltaGroup()
+        {}
+    }
+}
---
+++ src/DeltaObjectGenerator/Models/GroupConversionStatus.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace DeltaObjectGenerator.Models
+{
+    /// <summary>
+    /// Enum indicating status of conversion of <see cref="JObject"/> values into their associated 
+    /// properties.
+    /// </summary>
+    public enum GroupConversionStatus
+    {
+        /// <summary>
+        /// Indicates all deltas are convertible to the type of their associated property. 
+        /// </summary>
+        CompleteSuccess,
+
+        /// <summary>
+        /// Indicates some deltas are and some are not convertible to the type of their associated property.
+        /// </summary>
+        Partial,
+
+        /// <summary>
+        /// Indicates no deltas are convertible to the type of their associated property.
+        /// </summary>
+        CompleteFail
+    }
+}
